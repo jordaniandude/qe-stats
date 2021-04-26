8439b0b3173bbef8f4a38e94f6fee32b8899a7e5
Tariq Elouzeh <telouzeh@twitter.com>
2021-04-25 23:59:22 -0400

    Updates

---
+++ precache-manifest.dd24f46dd7200453e4300c1b7607e258.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "759b54b401babf7fe10cf82171e43f56",
+    "url": "/qe-stats/index.html"
+  },
+  {
+    "revision": "308e0eaeebb9dcf57c98",
+    "url": "/qe-stats/static/css/main.6e18a398.chunk.css"
+  },
+  {
+    "revision": "703806c52cd58ef33534",
+    "url": "/qe-stats/static/js/2.6e970887.chunk.js"
+  },
+  {
+    "revision": "308e0eaeebb9dcf57c98",
+    "url": "/qe-stats/static/js/main.2605b140.chunk.js"
+  },
+  {
+    "revision": "8de781fbd240f072189a",
+    "url": "/qe-stats/static/js/runtime-main.d761ff32.js"
+  },
+  {
+    "revision": "35c911edb1bbe1ed32752482cd33a72f",
+    "url": "/qe-stats/static/media/my-pic.35c911ed.jpg"
+  }
+]);
